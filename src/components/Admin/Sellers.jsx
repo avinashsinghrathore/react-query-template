f126fd475b5300d3be0e8c9ff045f41ed259be68
Avinash Singh Rathore <avinash@Avinashs-MacBook-Air.local>
2026-04-09 10:04:56 +0530

    fetch data using react query params

--- src/components/Admin/Sellers.jsx
+++ src/components/Admin/Sellers.jsx
@@ -3,11 +3,26 @@ import { useState } from "react";
 import useTodos from "../../hooks/useTodos";
 
 const Sellers = () => {
-  const { data: todos, error, isLoading } = useTodos();
+  const [userId, setUserId] = useState(null);
+  // console.log(userId);
+
+  const { data: todos, error, isLoading } = useTodos(userId);
 
   return (
     <>
       <h3>seller page</h3>
+      <h3>Todos Users</h3>
+      <select
+        onChange={(e) => {
+          setUserId(parseInt(e.target.value));
+        }}
+      >
+        <option value="">select</option>
+        <option value="1">User1</option>
+        <option value="2">User2</option>
+        <option value="3">User3</option>
+        <option value="4">User4</option>
+      </select>
       {isLoading && <h3>Loading...</h3>}
       {error && <em>{error.message}</em>}
       {todos?.map((todo) => (
--- src/hooks/useTodos.js
+++ src/hooks/useTodos.js
@@ -1,14 +1,19 @@
 import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
 
-const fetchTodos = () =>
-  axios
-    .get("https://jsonplaceholder.typicode.com/todos")
-    .then((res) => res.data);
+// const fetchTodos = () =>
+//   axios
+//     .get("https://jsonplaceholder.typicode.com/todos")
+//     .then((res) => res.data);
 
-const useTodos = () => {
+const useTodos = (userId) => {
+  const fetchTodos = () =>
+    axios
+      .get(`https://jsonplaceholder.typicode.com/todos`, { params: { userId } })
+      .then((res) => res.data);
+  // Users/1/todos - refrence
   return useQuery({
-    queryKey: ["todos"],
+    queryKey: userId ? ["users", userId, "todos"] : ["todos"],
     queryFn: fetchTodos,
   });
 };
